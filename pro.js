1bf4188a66935c94e76572ea3a9d473b24b8eebc
Idan-hub2023 <161726206+Idan-hub2023@users.noreply.github.com>
2024-03-10 20:56:01 +0200

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,39 @@
+function buy1() {
+    const cost = document.getElementById('cost1').textContent;
+    localStorage.setItem('price', cost);
+    window.location.href = 'form.html';
+
+}
+function buy2() {
+    window.location.href="form.html";
+    const cost = document.getElementById('cost2').textContent;
+    localStorage.setItem('price',cost);
+    
+}
+function buy3() {
+    window.location.href="form.html"
+   const cost = document.getElementById('cost3').textContent;
+    localStorage.setItem('price',cost)
+    
+}
+function buy4() {
+    window.location.href="form.html"
+   const cost = document.getElementById('cost4').textContent;
+    localStorage.setItem('price',cost)
+    
+    
+}
+function buy5() {
+    window.location.href="form.html"
+    const cost = document.getElementById('cost5').textContent;
+     localStorage.setItem('price',cost)
+     
+    
+}
+function buy6() {
+    window.location.href="form.html"
+   const cost = document.getElementById('cost6').textContent;
+    localStorage.setItem('price',cost)
+    
+    
+}
